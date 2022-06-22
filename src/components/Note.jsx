f26fa94bb274f4c7c513e5b60feede6835c4cbbe
Gorakh-Gupta <gorakhgupta23@gmail.com>
2022-06-22 10:26:11 +0530

    Initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+    return (
+        <div className="note">
+
+        <h3>This is Note title.</h3>
+        <p>This is Note conetent.</p>
+
+        </div>
+       
+    );
+}
+export default Note;
