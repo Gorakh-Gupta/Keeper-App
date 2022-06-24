d6044e93998fc7ae74678ce2c890820be8e3c25e
Gorakh-Gupta <gorakhgupta23@gmail.com>
2022-06-24 14:36:07 +0530

    Dynamically rendering the pages using props

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 
-function Note() {
-    return (
-        <div className="note">
-
-        <h3>This is Note title.</h3>
-        <p>This is Note conetent.</p>
-
-        </div>
-       
-    );
+function Note(props) {
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+    </div>
+  );
 }
+
 export default Note;
