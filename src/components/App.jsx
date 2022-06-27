ca29d337070307a4782c79b9ddd893c4b28ffa7c
Gorakh-Gupta <gorakhgupta23@gmail.com>
2022-06-27 17:39:02 +0530

    Dynamically add Titel and Content

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import Notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(note) {
+    setNotes((prevItems) => {
+      return [...prevItems, note];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      {Notes.map((note) => (
-        <Note key={note.key} title={note.title} content={note.content} />
-      ))}
+      <CreateArea onAdd={addNote} />
+
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+
       <Footer />
     </div>
   );
